d81471a67cfd9b4dc851850fc2dae7c457a76e95
jhlee <jhlee@spicusinc.com>
2024-05-23 01:13:37 +0900

    프로젝트 시작 :: 기본 타입 정의

---
+++ types/next-auth.d.ts
@@ -0,0 +1,13 @@
+import NextAuth, { Account } from 'next-auth'
+import { JWT } from 'next-auth/jwt'
+
+declare module 'next-auth' {
+  interface Session {
+    token: JWT
+  }
+
+  interface User {
+    provider: string,
+    username: string
+  }
+}
