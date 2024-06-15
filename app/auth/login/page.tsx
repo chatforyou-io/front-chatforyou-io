4c3ecebf16f7675edb63884ed65c6bc180eebbbb
junotb <sj202117@gmail.com>
2024-06-15 19:57:32 +0900

    디자인 변경 :: 마크업 수정

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,27 +1,23 @@
 'use client';
 
-import Header from "@/components/Header";
 import LoginButtons from "@/components/auth/LoginButtons";
 import LoginForm from "@/components/auth/LoginForm";
 
 export default function Page() {
   return (
-    <main className="flex flex-col items-center justify-center h-full bg-white dark:bg-gray-700 transition-colors duration-500">
-      <Header />
-      <div className="flex flex-col items-center justify-center gap-20 mt-12 p-4 w-full max-w-md h-full">
-        <div className="flex flex-col justify-center items-center gap-4 px-8 w-full">
-          <h1 className="text-gray-700 dark:text-gray-300 text-2xl">로그인</h1>
-          <LoginForm />
-        </div>
-        <div className="flex flex-col justify-center items-center gap-8 w-full mt-4">
-          <div className="relative flex justify-center border-b border-gray-700 dark:border-gray-300 w-full">
-            <p className="absolute -bottom-2.5 px-2 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300 text-sm transition-colors duration-500">또는 다음으로 로그인</p>
-          </div>
-          <div className="w-full px-20">
-            <LoginButtons />
-          </div>
-        </div>
+    <>
+      <div className="mx-auto">
+        <h1 className="text-gray-700 dark:text-gray-300 text-2xl">로그인</h1>
       </div>
-    </main>
+      <div className="mt-12 px-8 w-full">
+        <LoginForm />
+      </div>
+      <div className="relative flex justify-center border-b border-gray-700 dark:border-gray-300 mt-12 w-full">
+        <p className="absolute -bottom-2.5 px-2 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300 text-sm transition-colors duration-500">또는 다음으로 로그인</p>
+      </div>
+      <div className="w-full pt-12 px-20">
+        <LoginButtons />
+      </div>
+    </>
   );
 }
--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import { signIn } from "next-auth/react";
+import Link from "next/link";
 import { FormEvent, useRef, useState } from "react";
 
 export default function LoginForm() {
   const usernameRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
-  const [errorMessage, setErrorMessage] = useState<string>('');
   
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
@@ -26,7 +26,7 @@ export default function LoginForm() {
         errorMessage = '알 수 없는 오류로 로그인에 실패했습니다. 다시 시도해 주세요.';
       }
 
-      setErrorMessage(errorMessage);
+      alert(errorMessage);
       return;
     }
   }
@@ -35,34 +35,24 @@ export default function LoginForm() {
     <>
       <form
         onSubmit={handleSubmit}
-        className='flex flex-col gap-6 justify-center w-full text-gray-700 dark:text-gray-300 rounded'
+        className='w-full'
       >
-        <div className='flex flex-col items-center gap-2 w-full'>
-          <input
-            type='text'
-            ref={usernameRef}
-            className='border border-gray-700 dark:border-gray-300 p-2 w-full text-gray-700 outline-none rounded'
-            placeholder='이메일 주소' />
-          <input
-            type='password'
-            ref={passwordRef}
-            className='border border-gray-700 dark:border-gray-300 p-2 w-full text-gray-700 outline-none rounded'
-            placeholder='비밀번호' />
-        </div>
-        <div className="flex flex-col gap-2">
+        <input
+          type='text'
+          ref={usernameRef}
+          className='border border-gray-700 dark:border-gray-300 mt-2 p-2 w-full text-gray-700 outline-none rounded'
+          placeholder='이메일 주소' />
+        <input
+          type='password'
+          ref={passwordRef}
+          className='border border-gray-700 dark:border-gray-300 mt-2 p-2 w-full text-gray-700 outline-none rounded'
+          placeholder='비밀번호' />
+        <div className='flex justify-between mt-2 w-full'>
           <p className="hover:text-neutral-500 dark:hover:text-neutral-300 text-sm">비밀번호를 잊으셨나요?</p>
-          <button className='w-full border border-gray-700 dark:border-gray-300 p-2 font-bold hover:bg-gray-200 dark:hover:bg-gray-500 text-gray-700 dark:text-gray-300 rounded'>로그인</button>
+          <Link href="/auth/signup" className="hover:text-neutral-500 dark:hover:text-neutral-300 text-sm">가입하기</Link>
         </div>
+        <button className='w-full border border-gray-700 dark:border-gray-300 mt-2 p-2 font-bold hover:bg-gray-200 dark:hover:bg-gray-500 text-gray-700 dark:text-gray-300 rounded'>로그인</button>
       </form>
     </>
-  );
-}
-
-const LoginAlert = (errorMessage: string) => {
-  return (
-    <div
-      className='border border-gray-700 dark:border-gray-300 bg-white dark:bg-black text-black dark:text-white p-4 w-96 rounded'
-      role='alert'
-    >{errorMessage}</div>
   );
 }
