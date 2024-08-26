c7ce87ff5b83b6c158ebf4d3cc925443d856cafa
jhlee <jhlee@spicusinc.com>
2024-08-26 14:22:27 +0900

    빌드 :: userDelete 오류 수정

--- src/app/api/user/delete/route.ts
+++ src/app/api/user/delete/route.ts
@@ -2,11 +2,12 @@ import { userDelete } from '@/src/lib/auth';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export async function DELETE(req: NextRequest, params: { id: string }) {
+export async function DELETE(req: NextRequest, params: { id: string, pwd: string }) {
   const id = params.id;
+  const pwd = params.pwd;
 
   try {
-    const result = await userDelete(id);
+    const result = await userDelete(id, pwd);
     return NextResponse.json(result, { status: 201 });
   } catch (error) {
     console.error('fail delete user: ' + error);
