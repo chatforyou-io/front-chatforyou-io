96f7759aa90876122b02692f5816b27e83850455
jhlee <jhlee@spicusinc.com>
2024-09-07 14:48:32 +0900

    Revert "채팅방 :: 입장 페이지 작업"
    This reverts commit 35dfa48a2ea7dd526f7578958739b83e83eae149.

--- src/libs/openvidu.ts
+++ src/libs/openvidu.ts
@@ -41,6 +41,7 @@ const createToken = async (sessionId: string) => {
       'Authorization': 'Basic ' + OPENVIDU_CREDENTIALS,
     },
   }).then((response) => response.json());
+  console.log(data.token);
   return data.token; // The token
 }
 
