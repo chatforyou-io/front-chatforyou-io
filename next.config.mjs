1c4fb729c5772d6363e0b1663d3064ea29ef1191
jhlee <jhlee@spicusinc.com>
2024-09-21 17:23:57 +0900

    빌드 :: 기본 경로 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/chatforyouio/front/',
-  assetPrefix: '/chatforyouio/front/',
+  basePath: '/chatforyouio/front',
+  assetPrefix: '/chatforyouio/front',
 };
 
 export default nextConfig;
