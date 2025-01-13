e05f1680c94bb73bb37ab6d3d8143763af9d4579
jhlee <jhlee@spicusinc.com>
2025-01-13 12:32:43 +0900

    Mocks :: 채팅방 Mock 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,18 @@ import ChatroomCard from "@/src/components/cards/ChatroomCard";
 import ChatroomCreateForm from "@/src/components/forms/ChatroomCreateForm";
 import DashboardSidebar from "@/src/components/sidebars/DashboardSidebar";
 import { chatroomCreate, chatroomList } from "@/src/libs/chatroom";
+import chatroomMocks from "@/src/mocks/chatrooms.json";
 
 export default function Home() {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
   const router = useRouter();
   const [isPopup, setIsPopup] = useState<boolean>(false);
-  const [chatrooms, setChatrooms] = useState<Chatroom[]>([]);
+  const [chatrooms, setChatrooms] = useState<Chatroom[]>(chatroomMocks);
 
   const fetchChatrooms = useCallback(async () => {
     try {
       const response = await chatroomList();
-      setChatrooms(response.roomList);
+      setChatrooms([ ...chatrooms, ...(response.roomList || []) ]);
     } catch (error) {
       console.error("Failed to fetch chatrooms:", error);
     }
@@ -47,11 +48,11 @@ export default function Home() {
   }, [fetchChatrooms]);
 
   return (
-    <main className="w-full h-full bg-white">
+    <main className="size-full bg-white">
       <Header />
-      <div className="flex space-x-4 pt-24 pb-4 px-4 w-full h-full bg-gray-200">
+      <div className="flex space-x-4 pt-24 pb-4 px-4 size-full bg-gray-200 overflow-y-auto">
         <DashboardSidebar />
-        <div className="p-8 w-full h-full space-y-4">
+        <div className="p-8 size-full space-y-4">
           <div className="flex justify-between items-center">
             <h1 className="text-2xl font-bold">대시보드</h1>
             <div className="flex gap-3">
@@ -70,7 +71,7 @@ export default function Home() {
               </button>
             </div>
           </div>
-          <div className="flex gap-8 w-full h-full">
+          <div className="flex flex-wrap gap-8 size-fit">
             {chatrooms && chatrooms.length === 0 && <p>채팅방이 존재하지 않습니다.</p>}
             {chatrooms && chatrooms.map((chatroom, index) => {
               return (
@@ -81,8 +82,8 @@ export default function Home() {
       </div>
       {isPopup &&
         <>
-          <div className="absolute top-0 left-0 flex-center w-full h-full bg-black opacity-50"></div>
-          <div className="absolute top-0 left-0 flex-center w-full h-full">
+          <div className="absolute top-0 left-0 flex-center size-full bg-black opacity-50"></div>
+          <div className="absolute top-0 left-0 flex-center size-full">
             <ChatroomCreateForm onSubmit={handleCreateRoom} onClose={() => setIsPopup(false)} />
           </div>
         </>
