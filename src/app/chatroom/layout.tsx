a017915f3716a0d8125b2ee4d05b7637a1f2fb04
jhlee <jhlee@spicusinc.com>
2025-01-26 23:17:34 +0900

    Openvidu :: 컨텍스트 작업

--- src/app/chatroom/layout.tsx
+++ src/app/chatroom/layout.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import DimmedTemplate from "@/src/components/templates/DimmedTemplate";
+import Header from "@/src/components/Header";
+import OpenviduProvider from "@/src/contexts/OpenviduContext";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <DimmedTemplate>
-      {children}
-    </DimmedTemplate>
+    <main className="size-full bg-gray-200">
+      <Header />
+      <div className="pt-20 size-full">
+        <OpenviduProvider>
+          {children}
+        </OpenviduProvider>
+      </div>
+    </main>
   );
 }
