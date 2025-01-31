c510765fd60d00953f76cd70894e6deae6a93902
jhlee <jhlee@spicusinc.com>
2025-01-31 17:47:41 +0900

    디자인 :: Header 컴포넌트 위치 변경

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -5,7 +5,6 @@ import Header from "@/src/components/Header";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="size-full bg-gray-200">
-      <Header />
       <div className="pt-20 size-full">
         {children}
       </div>
--- src/app/chatroom/layout.tsx
+++ src/app/chatroom/layout.tsx
@@ -6,7 +6,6 @@ import OpenviduProvider from "@/src/contexts/OpenviduContext";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="size-full bg-gray-200">
-      <Header />
       <div className="pt-20 size-full">
         <OpenviduProvider>
           {children}
