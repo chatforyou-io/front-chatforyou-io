aca8cb27edd2688243fe90d3ab58709587931300
jhlee <jhlee@spicusinc.com>
2025-03-06 16:57:34 +0900

    기능 :: next-auth 의존성 제거

--- src/app/api/auth/callback/[provider]/route.ts
+++ src/app/api/auth/callback/[provider]/route.ts
@@ -18,7 +18,7 @@ export async function GET(request: Request, { params }: { params: RequestParams 
       const naverState = searchParams.get('state');
 
       // 토큰 발급
-      const { data: tokenData } = await axios.get('https://nid.naver.com/oauth2.0/token', {
+      const { data: tokenInfo } = await axios.get('https://nid.naver.com/oauth2.0/token', {
         params: {
           grant_type: 'authorization_code',
           client_id: naverClientId,
@@ -27,23 +27,26 @@ export async function GET(request: Request, { params }: { params: RequestParams 
           state: naverState,
         },
       });
-      const { access_token, refresh_token, token_type, expires_in } = tokenData;
 
       // 사용자 정보 조회
-      const { data: userInfoData } = await axios.get('https://openapi.naver.com/v1/nid/me', {
+      const { data: userInfo } = await axios.get('https://openapi.naver.com/v1/nid/me', {
         headers: {
-          Authorization: `Bearer ${access_token}`,
+          Authorization: `Bearer ${tokenInfo.access_token}`,
         },
       });
-      const { resultcode, message, response } = userInfoData;
-      if (resultcode !== '00') {
-        throw new Error(message);
+      if (userInfo.resultcode !== '00') {
+        throw new Error(userInfo.message);
       }
 
       // 사용자 정보 추출
-      const { id, nickname, profile_image, age, gender, email, mobile, mobile_e164, name, birthday, birthyear } = response;
-      console.log(id, nickname, profile_image, age, gender, email, mobile, mobile_e164, name, birthday, birthyear);
-      break;
+      /*
+      const { isSuccess, userData } = await socialLogin(userInfo.response.provider, userInfo.response.id, userInfo.response.email, userInfo.response.name, userInfo.response.nickname);
+
+      if (!isSuccess) {
+        return Response.json({ error: '로그인에 실패했습니다.' }, { status: 400 });
+      }
+      */
+      return Response.json(userInfo, { status: 200 });
     case "kakao":
       break;
     case "google":
