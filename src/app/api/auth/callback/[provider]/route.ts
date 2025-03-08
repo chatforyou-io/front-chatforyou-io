b5a4255de904830a10f248d73ecae13e2f7e59b9
jhlee <jhlee@spicusinc.com>
2025-03-09 02:00:30 +0900

    기능 :: next-auth 의존성 제거

--- src/app/api/auth/callback/[provider]/route.ts
+++ src/app/api/auth/callback/[provider]/route.ts
@@ -1,10 +1,10 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 import axios from "axios";
 import jwt from "jsonwebtoken";
 import { socialSignIn } from "@/src/libs/auth";
 import { cookies } from "next/headers";
 
-const JWT_SECRET = process.env.JWT_SECRET || '';
+const JWT_SECRET = process.env.JWT_SECRET || "";
 
 interface RequestParams {
   provider: string;
@@ -13,7 +13,7 @@ interface RequestParams {
 export async function GET(request: NextRequest, { params }: { params: RequestParams }) {
   const { provider } = params;
 
-  // URL에서 'code' 파라미터 추출
+  // URL에서 "code" 파라미터 추출
   const { searchParams } = request.nextUrl;
 
   switch (provider) {
@@ -21,13 +21,13 @@ export async function GET(request: NextRequest, { params }: { params: RequestPar
       const naverClientId = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID;
       const naverClientSecret = process.env.NAVER_CLIENT_SECRET;
       const naverRedirectUri = process.env.NEXT_PUBLIC_NAVER_REDIRECT_URI;
-      const naverCode = searchParams.get('code');
-      const naverState = searchParams.get('state');
+      const naverCode = searchParams.get("code");
+      const naverState = searchParams.get("state");
 
       // 토큰 발급
-      const { data: naverTokenInfo } = await axios.get('https://nid.naver.com/oauth2.0/token', {
+      const { data: naverTokenInfo } = await axios.get("https://nid.naver.com/oauth2.0/token", {
         params: {
-          grant_type: 'authorization_code',
+          grant_type: "authorization_code",
           client_id: naverClientId,
           client_secret: naverClientSecret,
           redirect_uri: naverRedirectUri,
@@ -37,7 +37,7 @@ export async function GET(request: NextRequest, { params }: { params: RequestPar
       });
 
       // 사용자 정보 조회
-      const { data: naverUserInfo } = await axios.get('https://openapi.naver.com/v1/nid/me', {
+      const { data: naverUserInfo } = await axios.get("https://openapi.naver.com/v1/nid/me", {
         headers: {
           Authorization: `Bearer ${naverTokenInfo.access_token}`,
         },
@@ -48,55 +48,37 @@ export async function GET(request: NextRequest, { params }: { params: RequestPar
 
       // 로그인 실패 시
       if (!naverIsSuccess) {
-        return NextResponse.json({ error: '로그인에 실패했습니다.' }, { status: 400 });
+        return NextResponse.json({ error: "로그인에 실패했습니다." }, { status: 400 });
       }
 
       // 사용자 데이터가 존재하지 않을 경우
       if (!naverUserData) {
-        return NextResponse.json({ error: '사용자 데이터가 존재하지 않습니다.' }, { status: 400 });
+        return NextResponse.json({ error: "사용자 데이터가 존재하지 않습니다." }, { status: 400 });
       }
 
       // 토큰이 존재하지 않을 경우
       if (!naverAccessToken || !naverRefreshToken) {
-        return NextResponse.json({ error: '토큰이 존재하지 않습니다.' }, { status: 400 });
+        return NextResponse.json({ error: "토큰이 존재하지 않습니다." }, { status: 400 });
       }
 
       // session 토큰 생성
       const naverSessionToken = jwt.sign(naverUserData, JWT_SECRET, { expiresIn: "1h" });
 
-      // AccessToken 쿠키 설정
-      cookies().set("AccessToken", naverAccessToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
-      });
+      // 쿠키 설정
+      setAuthCookies(naverUserData, naverAccessToken, naverRefreshToken, naverSessionToken);
 
-      // RefreshToken 쿠키 설정
-      cookies().set("RefreshToken", naverRefreshToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
-      });
-
-      // SessionToken 쿠키 설정
-      cookies().set("SessionToken", naverSessionToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
-      });
-
-      return NextResponse.redirect(new URL('/chatforyouio/front', request.url));
+      return NextResponse.redirect(new URL("/chatforyouio/front", request.url));
     case "kakao":
       const kakaoClientId = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
       const kakaoClientSecret = process.env.KAKAO_CLIENT_SECRET;
       const kakaoRedirectUri = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
-      const kakaoCode = searchParams.get('code');
-      const kakaoState = searchParams.get('state');
+      const kakaoCode = searchParams.get("code");
+      const kakaoState = searchParams.get("state");
 
       // 토큰 발급
-      const { data: kakaoTokenInfo } = await axios.get('https://kauth.kakao.com/oauth/token', {
+      const { data: kakaoTokenInfo } = await axios.get("https://kauth.kakao.com/oauth/token", {
         params: {
-          grant_type: 'authorization_code',
+          grant_type: "authorization_code",
           client_id: kakaoClientId,
           client_secret: kakaoClientSecret,
           redirect_uri: kakaoRedirectUri,
@@ -106,7 +88,7 @@ export async function GET(request: NextRequest, { params }: { params: RequestPar
       });
 
       // 사용자 정보 조회
-      const { data: kakaoUserInfo } = await axios.get('https://kapi.kakao.com/v2/user/me', {
+      const { data: kakaoUserInfo } = await axios.get("https://kapi.kakao.com/v2/user/me", {
         headers: {
           Authorization: `Bearer ${kakaoTokenInfo.access_token}`,
         },
@@ -117,54 +99,36 @@ export async function GET(request: NextRequest, { params }: { params: RequestPar
 
       // 로그인 실패 시
       if (!kakaoIsSuccess) {
-        return NextResponse.json({ error: '로그인에 실패했습니다.' }, { status: 400 });
+        return NextResponse.json({ error: "로그인에 실패했습니다." }, { status: 400 });
       }
 
       // 사용자 데이터가 존재하지 않을 경우
       if (!kakaoUserData) {
-        return NextResponse.json({ error: '사용자 데이터가 존재하지 않습니다.' }, { status: 400 });
+        return NextResponse.json({ error: "사용자 데이터가 존재하지 않습니다." }, { status: 400 });
       }
 
       // 토큰이 존재하지 않을 경우
       if (!kakaoAccessToken || !kakaoRefreshToken) {
-        return NextResponse.json({ error: '토큰이 존재하지 않습니다.' }, { status: 400 });
+        return NextResponse.json({ error: "토큰이 존재하지 않습니다." }, { status: 400 });
       }
 
       // session 토큰 생성
       const kakaoSessionToken = jwt.sign(kakaoUserData, JWT_SECRET, { expiresIn: "1h" });
 
-      // AccessToken 쿠키 설정
-      cookies().set("AccessToken", kakaoAccessToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
-      });
+      // 쿠키 설정
+      setAuthCookies(kakaoUserData, kakaoAccessToken, kakaoRefreshToken, kakaoSessionToken);
 
-      // RefreshToken 쿠키 설정
-      cookies().set("RefreshToken", kakaoRefreshToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
-      });
-
-      // SessionToken 쿠키 설정
-      cookies().set("SessionToken", kakaoSessionToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
-      });
-
-      return NextResponse.redirect(new URL('/chatforyouio/front', request.url));
+      return NextResponse.redirect(new URL("/chatforyouio/front", request.url));
     case "google":
       const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
       const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
       const googleRedirectUri = process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI;
-      const googleCode = searchParams.get('code');
-      const googleState = searchParams.get('state');
+      const googleCode = searchParams.get("code");
+      const googleState = searchParams.get("state");
 
       // 토큰 발급  
-      const { data: googleTokenInfo } = await axios.post('https://oauth2.googleapis.com/token', {
-        grant_type: 'authorization_code',
+      const { data: googleTokenInfo } = await axios.post("https://oauth2.googleapis.com/token", {
+        grant_type: "authorization_code",
         client_id: googleClientId,
         client_secret: googleClientSecret,
         redirect_uri: googleRedirectUri,
@@ -173,58 +137,60 @@ export async function GET(request: NextRequest, { params }: { params: RequestPar
       });
 
       // 사용자 정보 조회
-      const { data: googleUserInfo } = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
+      const { data: googleUserInfo } = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
         headers: {
           Authorization: `Bearer ${googleTokenInfo.access_token}`,
         },
       });
 
       // 로그인 요청
-      const { isSuccess: googleIsSuccess, userData: googleUserData, accessToken: googleAccessToken, refreshToken: googleRefreshToken } = await socialSignIn(provider, googleUserInfo.sub, googleUserInfo.email, googleUserInfo.name, '');
+      const { isSuccess: googleIsSuccess, userData: googleUserData, accessToken: googleAccessToken, refreshToken: googleRefreshToken } = await socialSignIn(provider, googleUserInfo.sub, googleUserInfo.email, googleUserInfo.name, "");
 
       // 로그인 실패 시
       if (!googleIsSuccess) {
-        return NextResponse.json({ error: '로그인에 실패했습니다.' }, { status: 400 });
+        return NextResponse.json({ error: "로그인에 실패했습니다." }, { status: 400 });
       }
 
       // 사용자 데이터가 존재하지 않을 경우
       if (!googleUserData) {
-        return NextResponse.json({ error: '사용자 데이터가 존재하지 않습니다.' }, { status: 400 });
+        return NextResponse.json({ error: "사용자 데이터가 존재하지 않습니다." }, { status: 400 });
       }
 
       // 토큰이 존재하지 않을 경우
       if (!googleAccessToken || !googleRefreshToken) {
-        return NextResponse.json({ error: '토큰이 존재하지 않습니다.' }, { status: 400 });
+        return NextResponse.json({ error: "토큰이 존재하지 않습니다." }, { status: 400 });
       }
 
       // session 토큰 생성
       if (!googleUserData || !googleAccessToken || !googleRefreshToken) {
-        return NextResponse.json({ error: '토큰이 존재하지 않습니다.' }, { status: 400 });
+        return NextResponse.json({ error: "토큰이 존재하지 않습니다." }, { status: 400 });
       }
 
       const googleSessionToken = jwt.sign(googleUserData as object, JWT_SECRET, { expiresIn: "1h" });
 
-      // AccessToken 쿠키 설정
-      cookies().set("AccessToken", googleAccessToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
-      });
+      // 쿠키 설정
+      setAuthCookies(googleUserData, googleAccessToken, googleRefreshToken, googleSessionToken);
 
-      // RefreshToken 쿠키 설정
-      cookies().set("RefreshToken", googleRefreshToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
-      });
-
-      // SessionToken 쿠키 설정
-      cookies().set("SessionToken", googleSessionToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
-      });
-
-      return NextResponse.redirect(new URL('/chatforyouio/front', request.url));
+      return NextResponse.redirect(new URL("/chatforyouio/front", request.url));
   }    
+}
+
+function setAuthCookies(userData: any, accessToken: string, refreshToken: string, sessionToken: string) {
+  cookies().set("AccessToken", accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+  });
+
+  cookies().set("RefreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+  });
+
+  cookies().set("SessionToken", sessionToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+  });
 }
