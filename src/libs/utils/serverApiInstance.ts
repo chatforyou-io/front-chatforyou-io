8e2de656a92590b356570e6e45b7dbfe914bec6f
jhlee <jhlee@spicusinc.com>
2025-03-15 17:35:07 +0900

    버그 :: 토큰 만료시 로그인 페이지로 이동

--- src/libs/utils/serverApiInstance.ts
+++ src/libs/utils/serverApiInstance.ts
@@ -1,6 +1,10 @@
+"use server";
+
 import axios from "axios";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 const authHost = process.env.NEXT_PUBLIC_API_DOMAIN;
 
 const serverApiInstance = axios.create({
@@ -21,6 +25,17 @@ serverApiInstance.interceptors.request.use(
     return config;
   },
   (error) => Promise.reject(error)
+);
+
+serverApiInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      redirect(`${basePath}/auth/login`);
+    }
+
+    return Promise.reject(error);
+  }
 );
 
 export default serverApiInstance;
