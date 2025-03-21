46a9c9cfbbac0d4422f970d7ad711d509dc2216d
jhlee <jhlee@spicusinc.com>
2025-03-21 22:37:48 +0900

    버그 :: 방 생성일 미노출 버그

--- src/libs/sses/chatroomInfo.ts
+++ src/libs/sses/chatroomInfo.ts
@@ -7,26 +7,22 @@ export const connectChatroomInfoSSE = (
   const eventSource = new EventSource(`${apiDomain}/chatforyouio/sse/chatroom/${sessionId}/user/${idx}`);
 
   eventSource.addEventListener("connectionStatus", (event) => {
-    console.log("connectionStatus:", event);
     const { data } = JSON.parse(event.data);
     callbacks.onConnectionStatus?.(data);
   });
 
   eventSource.addEventListener("keepAlive", (event) => {
-    console.log("keepAlive:", event);
     const { data } = JSON.parse(event.data);
     callbacks.onKeepAlive?.(data);
   });
 
   eventSource.addEventListener("updateChatroomInfo", (event) => {
-    console.log("updateChatroomInfo:", event);
     const { data } = JSON.parse(event.data);
     const chatroom = typeof data === "string" ? JSON.parse(data) : data;
     callbacks.onUpdateChatroomInfo?.(chatroom);
   });
 
   eventSource.onerror = (error) => {
-    console.log("onerror:", error);
     callbacks.onError?.(error);
     eventSource.close();
   };
--- src/libs/sses/chatroomList.ts
+++ src/libs/sses/chatroomList.ts
@@ -7,26 +7,22 @@ export const connectChatroomListSSE = (
   const eventSource = new EventSource(`${apiDomain}/chatforyouio/sse/chatroom/list/${idx}`);
 
   eventSource.addEventListener("connectionStatus", (event) => {
-    console.log("connectionStatus:", event);
     const { data } = JSON.parse(event.data);
     callbacks.onConnectionStatus?.(data);
   });
 
   eventSource.addEventListener("keepAlive", (event) => {
-    console.log("keepAlive:", event);
     const { data } = JSON.parse(event.data);
     callbacks.onKeepAlive?.(data);
   });
 
   eventSource.addEventListener("updateChatroomList", (event) => {
-    console.log("updateChatroomList:", event);
     const { data } = JSON.parse(event.data);
     const chatrooms = typeof data === "string" ? JSON.parse(data) : data;
     callbacks.onUpdateChatroomList?.(chatrooms);
   });
 
   eventSource.onerror = (error) => {
-    console.log("onerror:", error);
     callbacks.onError?.(error);
     eventSource.close();
   };
